aa080029e11ccad9f9d27b780d5f78c8cc8f0402
kiezmor <kiezmor60@gmail.com>
2018-08-15 23:57:31 +0200

    basic server done

---
+++ web_server/web_server.ino
@@ -0,0 +1,55 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <SD.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 45);
+EthernetServer server(80);
+File webFile;
+
+void setup() {
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.begin(9600);
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(4)) {
+    Serial.println("ERROR - SD card initialization failed!");
+    return;
+  }
+  Serial.println("SUCCESS - SD card initialized.");
+  if (!SD.exists("index.htm")) {
+    Serial.println("ERROR - Can't find index.htm file!");
+    return;
+  } Serial.println("SUCCESS - Found index.htm file.");
+}
+
+void loop() {
+  EthernetClient client = server.available();
+  if (client) {
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        if (c == '\n' && currentLineIsBlank) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println();
+          webFile = SD.open("index.htm");
+          if (webFile) {
+            while (webFile.available()) {
+              client.write(webFile.read());
+            } webFile.close();
+          } break;
+        }
+        if (c == '\n') {
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    delay(1);
+    client.stop();
+  }
+}
